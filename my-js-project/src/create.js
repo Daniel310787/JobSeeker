b6e56493d16815da077b0f71bc9f6fe1ecfd0924
Daniel Alvarado <Daniel Alvarado daniel310787@hotmail.com>
2021-03-19 15:03:22 +0100

    .

--- my-js-project/src/create.js
+++ my-js-project/src/create.js
@@ -1,22 +1,5 @@
 
 
-// fetch('https://jooble.org/api/281e5e02-f47b-41cb-a0e7-7ea5a33f21e1', {
-//   method: 'POST',
-//   headers: { 'Content-Type': 'application/json' },
-//   body: JSON.stringify(json),
-// })
-// .then(response => response.json())
-// .then(data => console.log(data))
-
-// var json = {
-//     "keywords": "account manager",
-//     "location": "London",
-//     "radius": "50",
-//     "salary": "200000",
-//     "page": "1"
-//  }
-
-
 var url = "https://jooble.org/api/";
 var key = "281e5e02-f47b-41cb-a0e7-7ea5a33f21e1";
 var params = "{ keywords: 'it', location: 'Rome'}"
