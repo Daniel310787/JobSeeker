617a3954874541b6dbe5bfcf225ec663c3996b73
Daniel Alvarado <Daniel Alvarado daniel310787@hotmail.com>
2021-03-29 14:45:14 +0200

    add

--- my-js-project/src/listofJobs.js
+++ my-js-project/src/listofJobs.js
@@ -2,9 +2,26 @@ var urlParams = new URLSearchParams(window.location.search);
 
 var locs = urlParams.get('location');
 var jobs = urlParams.get('job')
-console.log(locs)
-console.log(jobs)
 
+var str = locs;
+var arr = str.split(',');
+arr = arr.splice(0, arr.length - 1)
+console.log(arr.join(','))
+
+fetch(`http://api.openweathermap.org/data/2.5/weather?q=${locs}&appid=a649f48c0d27ec05c0d86837b49029b1&units=metric`)
+.then(response => response.json())  
+.then(data => {
+	var lat = data.coord.lat
+	var lon = data.coord.lon
+		mapboxgl.accessToken = 'pk.eyJ1IjoiZGFuaWVsMzEwNzg3IiwiYSI6ImNrbTRzZXR2ZzA3bHgycG93YzI1dmFyb3kifQ.av0j5J9UNRTWdRs9zXR8cg';
+		var map = new mapboxgl.Map({
+		container: "map", 
+		style: 'mapbox://styles/mapbox/streets-v11',
+		center: [Math.floor(lon), Math.floor(lat)],
+		zoom: 7,
+	});
+
+})
 function Jobs() {
 	document.querySelectorAll("div").forEach(element => {
     element.remove()
@@ -49,33 +66,26 @@ function Jobs() {
 				document.getElementById("results").appendChild(name2)
 				document.getElementById("image").appendChild(fig)
 				document.getElementById("results").appendChild(div)
-				
+
 				var str = element.location;
 				var arr = str.split(',');
 				arr = arr.splice(0, arr.length - 1)
 				console.log(arr.join(','))
 				
-				fetch(`http://api.openweathermap.org/data/2.5/weather?q=${arr}&appid=a649f48c0d27ec05c0d86837b49029b1&units=metric`)
-				.then(response => response.json())  
-				.then(data =>{
-				var lat = data.coord.lat
-				var lon = data.coord.lon
-				console.log(data)
-				mapboxgl.accessToken = 'pk.eyJ1IjoiZGFuaWVsMzEwNzg3IiwiYSI6ImNrbTRzZXR2ZzA3bHgycG93YzI1dmFyb3kifQ.av0j5J9UNRTWdRs9zXR8cg';
-				var map = new mapboxgl.Map({
-				container: "map", 
-				style: 'mapbox://styles/mapbox/streets-v11',
-				center: [Math.floor(lon), Math.floor(lat)],
-				zoom: 7 
-			});
+				var marker = new mapboxgl.Marker()
+				.setLngLat([Math.floor(lon), Math.floor(lat)])
+				.addTo(map);
+				
+				
 			})
-			});
 		}
-			
-		
 	}
 	http.send(params);
 }
+
+
+
+
 window.addEventListener("load", Jobs)
 
 
@@ -129,6 +139,8 @@ function Jobs1() {
 		}
 	}
 	http.send(params);
+
 }
+
 
 document.getElementById("btn").addEventListener("click", Jobs1)
